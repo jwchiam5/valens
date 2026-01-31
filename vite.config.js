961f3458dc3847d650213b82191c5494c5d9d03c
joonwee <142730167+jwchiam5@users.noreply.github.com>
2026-01-31 10:38:35 +0800

    'test'

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-    port: 3000,
-  },
+  // server: {
+  //   open: true,
+  //   port: 3000,
+  // },
+  base: '/valens/',
   build: {
-    outDir: 'dist',
+    outDir: '../docs',
   },
 });
